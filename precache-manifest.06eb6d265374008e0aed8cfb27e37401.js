9cca49cd4b50f83b107481e505c224a821284356
Yaron <yaronl219@gmail.com>
2020-10-06 09:49:50 +0300

    Updates

---
+++ precache-manifest.06eb6d265374008e0aed8cfb27e37401.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef12a2a69a158e48f1f9e2a92a5bd16",
+    "url": "/yaronl219/myteam/index.html"
+  },
+  {
+    "revision": "cb8e0aab8eb5bbcb2725",
+    "url": "/yaronl219/myteam/static/js/2.d496c058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yaronl219/myteam/static/js/2.d496c058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c157e09727bb7e59c11e",
+    "url": "/yaronl219/myteam/static/js/main.77a2aad1.chunk.js"
+  },
+  {
+    "revision": "6eb8268250e221c59b67",
+    "url": "/yaronl219/myteam/static/js/runtime-main.fec4c3ca.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/yaronl219/myteam/precache-manifest.06eb6d265374008e0aed8cfb27e37401.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yaronl219/myteam/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
